d14d78eb0ed1aa4661e9a8fdcd28469345d7b45b
Nizar Khalife Iglesias <khalifenizar@gmail.com>
2016-09-20 09:10:32 -0400

    Sandwichr: Add solution

---
+++ solutions/sandwichr/app/controllers/sandwiches_controller.rb
@@ -0,0 +1,69 @@
+class SandwichesController < ApplicationController
+
+  def index
+    sandwiches_array = Sandwich.all
+    render json: sandwiches_array
+  end
+
+  def create
+    the_sandwich = Sandwich.create(sandwich_params)
+    render json: the_sandwich
+  end
+
+  def show
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      # render json: { sandwich: the_sandwich, ingredients: the_sandwich.ingredients }
+      render json: the_sandwich.to_json(include: [:ingredients])
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+  def update
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      the_sandwich.update(sandwich_params)
+      render json: the_sandwich
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+  def destroy
+    the_sandwich = Sandwich.find_by(id: params[:id])
+
+    if the_sandwich
+      the_sandwich.destroy
+      head :no_content
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+  # post "/sandwiches/:id/ingredients/add"
+  def add_ingredient
+    # SandwichIngredients.create(sandwich_id: params[:id], ingredient_id: params[:ingredient_id])
+
+    the_sandwich = Sandwich.find_by(id: params[:id])
+    the_ingredient = Ingredient.find_by(id: params[:ingredient_id])
+
+    if the_sandwich && the_ingredient
+      the_sandwich.ingredients.push( the_ingredient )
+
+      render json: the_sandwich.to_json(include: [:ingredients])
+    else
+      render status: 404, json: { error: "Sandwich #{params[:id]} not found" }
+    end
+  end
+
+
+  private
+
+  def sandwich_params
+    params.require(:sandwich).permit(:name, :bread_type)
+  end
+
+end
